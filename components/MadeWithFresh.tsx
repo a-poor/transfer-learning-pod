1ec0650a1039326642b014945529b76af82c0a51
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-07-09 22:45:21 -0700

    Basic landing page layout
    - A basic landing page for the podcast
    - Has links to the podcast on apple/spotify/rss
    - No episode details
    - Has link to my personal site

---
+++ components/MadeWithFresh.tsx
@@ -0,0 +1,12 @@
+export default function MadeWithFresh() {
+  return (
+    <a href="https://fresh.deno.dev" className="fixed bottom-5 right-5 z-10">
+      <img
+        width="197"
+        height="37"
+        src="https://fresh.deno.dev/fresh-badge.svg"
+        alt="Made with Fresh"
+      />
+    </a>
+  );
+}
